0535bc9cd765b5629127251b34480ae7bccec288
InzaSanam08 <inzasanam7@gmail.com>
2025-07-21 04:19:31 -0700

    Animated Game

---
+++ app.js
@@ -0,0 +1,52 @@
+var modal1 = document.getElementById("modal1");
+var speed = 5;
+var flag = false;
+
+function characterMovement(takeOrder) {
+    if (takeOrder === 'forward') {
+        speed += 5;
+        modal1.style.left = speed + 'px';
+        if (!flag) {
+            // modal1.src = 'backGround/characters/kokonoe-blazblue.gif';
+        } flag = true;
+    } else if (takeOrder === 'left') {
+        speed -= 5;
+        modal1.style.left = speed + 'px';
+        if (flag) {
+            modal1.src = 'backGround/characters/aaa.gif';
+        } flag = false;
+    }
+    // else if (takeOrder === 'down') {
+    //     console.log('Moving character down');
+    // } else if (takeOrder === 'left') {
+    //     console.log('Moving character left');
+    // } else if (takeOrder === 'up') {
+    //     console.log('Moving character right');
+    // } else {
+    // }
+}
+
+
+function moveCharacter(event) {
+    console.log(event.key);
+
+    if (event.key === 'ArrowRight') {
+        characterMovement("forward");
+    }
+    // else if (event.key === 'ArrowDown') {
+    //     characterMovement('down');
+    // }
+     else if (event.key === 'ArrowLeft') {
+        characterMovement('left');
+    } 
+    // else if (event.key === 'ArrowUp') {
+    //     characterMovement('up');
+
+    // }
+    //  else {
+    //     console.log('Invalid key pressed');
+    // }
+}
+
+
+window.addEventListener('keydown', moveCharacter);
