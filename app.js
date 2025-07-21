bfcaa8af66cef8308dfc4a83176c1670b0c76cc0
InzaSanam08 <inzasanam7@gmail.com>
2025-07-21 09:53:14 -0700

    Animated Game

--- app.js
+++ app.js
@@ -1,51 +1,32 @@
-var modal1 = document.getElementById("modal1");
+var characterImage = document.getElementById("character-image");
 var speed = 5;
 var flag = false;
 
 function characterMovement(takeOrder) {
     if (takeOrder === 'forward') {
         speed += 5;
-        modal1.style.left = speed + 'px';
+        characterImage.style.left = speed + 'px';
         if (!flag) {
-            modal1.src = 'backGround/characters/a.gif';
+            characterImage.src = 'backGround/characters/character1.gif';
         } flag = true;
     } else if (takeOrder === 'left') {
         speed -= 5;
-        modal1.style.left = speed + 'px';
+        characterImage.style.left = speed + 'px';
         if (flag) {
-            modal1.src = 'backGround/characters/aaa.gif';
+            characterImage.src = 'backGround/characters/character2.gif';
         } flag = false;
-    }
-    else if (takeOrder === 'down') {
-        console.log('Moving character down');
-    } else if (takeOrder === 'left') {
-        console.log('Moving character left');
-    } else if (takeOrder === 'up') {
-        console.log('Moving character right');
-    } else {
     }
 }
 
 
 function moveCharacter(event) {
     console.log(event.key);
-
     if (event.key === 'ArrowRight') {
         characterMovement("forward");
     }
-    // else if (event.key === 'ArrowDown') {
-    //     characterMovement('down');
-    // }
     else if (event.key === 'ArrowLeft') {
         characterMovement('left');
     }
-    // else if (event.key === 'ArrowUp') {
-    //     characterMovement('up');
-
-    // }
-    //  else {
-    //     console.log('Invalid key pressed');
-    // }
 }
 
 
